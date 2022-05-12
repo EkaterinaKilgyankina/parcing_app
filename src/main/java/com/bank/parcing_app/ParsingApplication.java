ffe3e4917521b54c30a1d5c8d39bd0cebdc79b97
ekaterina_kilgiankina <kilgyankina.ekaterina@gmail.com>
2022-05-12 14:06:04 +0300

    banner blocking added exit app logic added

--- src/main/java/com/bank/parcing_app/ParsingApplication.java
+++ src/main/java/com/bank/parcing_app/ParsingApplication.java
@@ -1,26 +1,31 @@
 package com.bank.parcing_app;
 
+import com.bank.parcing_app.service.ExecutionService;
 import com.bank.parcing_app.service.ServiceMain;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.stream.Stream;
 
 @SpringBootApplication
 @AllArgsConstructor
+@EnableScheduling
 class ParsingApplication implements CommandLineRunner {
     private final ServiceMain serviceMain;
+    private final ExecutionService  executionService;
 
     public static void main(String[] args) {
-        SpringApplication.run(ParsingApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(ParsingApplication.class, args);
     }
 
     @Override
     public void run(String... args) {
         Stream.of(args)
                 .forEach(serviceMain::doWork);
+        executionService.shutdown();
     }
-
 }
--- src/main/java/com/bank/parcing_app/service/ExecutionService.java
+++ src/main/java/com/bank/parcing_app/service/ExecutionService.java
@@ -1,5 +1,6 @@
 package com.bank.parcing_app.service;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.ExecutorService;
@@ -11,5 +12,16 @@ public class ExecutionService {
 
     public void submit(Runnable runnable) {
         pool.submit(runnable);
+    }
+
+    public void shutdown() {
+        pool.shutdown();
+    }
+
+    @Scheduled(fixedRate = 500L)
+    public void isShutDown() {
+        if (pool.isShutdown()) {
+            System.exit(0);
+        }
     }
 }
